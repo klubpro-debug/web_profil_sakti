bd399899e995893745f386c3a59cc2b66641f688
rudani37 <rudani097@gmail.com>
2019-09-20 15:54:07 +0700

    edit partials

---
+++ application/views/partials_depan/head.php
@@ -0,0 +1,26 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?php echo SITE_NAME .": ". ucfirst($this->uri->segment(1))?></title>
+    <link rel="shorcut icon" href="<?php echo base_url().'theme/images/icon.png'?>">
+    <link rel="shorcut icon" type="text/css" href="<?php echo base_url().'theme/images/logo-dark.png'?>">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.min.css'?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/font-awesome.min.css'?>">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/simple-line-icons.css'?>">
+    <!-- Slider / Carousel -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick-theme.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/owl.carousel.min.css'?>">
+    <!-- Main CSS -->
+    <link href="<?php echo base_url().'theme/css/style.css'?>" rel="stylesheet">
+    <?php
+        function limit_words($string, $word_limit){
+            $words = explode(" ",$string);
+            return implode(" ",array_splice($words,0,$word_limit));
+        }
+    ?>
