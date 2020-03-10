f7cd52d810203b7f4cab34ad821db2a6520402d0
Fajrizky <muhammadfajrizky@gmail.com>
2020-03-10 22:30:59 +0700

    reset

--- application/models/M_kelas.php
+++
@@ -1,9 +0,0 @@
-<?php
-class M_kelas extends CI_Model{
-
-	function get_all_kelas(){
-		$hsl=$this->db->query("SELECT * FROM tbl_kelas");
-		return $hsl;
-	}
-
-}
--- application/views/partials_admin/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 1.0
-    </div>
-    <strong>Copyright &copy; 2019 UKM SAKTI PALANGKA RAYA</a>.</strong> All rights reserved.
-  </footer>
