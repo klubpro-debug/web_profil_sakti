27fdefda5ef0fd69eb12119c7ec02d9c13195c1a
anastiain <anastiain@gmail.com>
2020-06-02 20:49:21 +0700

    robotika g pakai bahasa pemrograman

--- application/views/depan/v_robo.php
+++ application/views/depan/v_robo.php
@@ -104,76 +104,8 @@
     </div>
     <!----------------------------- AKHIR JUDUL  ----------------------------->
 
-    <!-- menu feature -->
-    <!----------------------------- AWAL JUDUL  ----------------------------->
     <div class="clear"></div>
-    <div class="divider divider-short divider-center"><i class="icon-circle"></i></div>
-    <div id="section-bahasaPemprograman" class="heading-block title-center page-section">
-        <h2>Bahasa Pemprograman</h2>
-        <span>“I think everybody in this country should learn how to program a computer because it teaches you how to think.” — Steve Jobs dari Apple Inc.</span>
-    </div>
-    <!----------------------------- AKHIR JUDUL  ----------------------------->
-    <section id="content">
-        <div class="content-wrap">
-            <div class="container clearfix">
-                <div class="col_one_third">
-                    <div class="feature-box fbox-border fbox-effect">
-                        <div class="fbox-icon">
-                            <a href="#"><i class="icon-screen i-alt"></i></a>
-                        </div>
-                        <h3>Responsive Layout</h3>
-                        <p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
-                    </div>
-                </div>
-                <div class="col_one_third">
-                    <div class="feature-box fbox-border fbox-effect">
-                        <div class="fbox-icon">
-                            <a href="#"><i class="icon-eye i-alt"></i></a>
-                        </div>
-                        <h3>Retina Ready Graphics</h3>
-                        <p>Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Retina Icons, Fonts &amp; all others graphics are optimized.</p>
-                    </div>
-                </div>
-                <div class="col_one_third col_last">
-                    <div class="feature-box fbox-border fbox-effect">
-                        <div class="fbox-icon">
-                            <a href="#"><i class="icon-beaker i-alt"></i></a>
-                        </div>
-                        <h3>Powerful Performance</h3>
-                        <p>Canvas includes tons of optimized code that are completely customizable and deliver unmatched fast performance.</p>
-                    </div>
-                </div>
-                <div class="clear"></div>
-                <div class="col_one_third nobottommargin">
-                    <div class="feature-box fbox-border fbox-effect">
-                        <div class="fbox-icon">
-                            <a href="#"><i class="icon-stack i-alt"></i></a>
-                        </div>
-                        <h3>Premium Sliders Included</h3>
-                        <p>Canvas included 20+ custom designed Slider Pages with Premium Sliders like Layer, Revolution, Swiper &amp; others.</p>
-                    </div>
-                </div>
-                <div class="col_one_third nobottommargin">
-                    <div class="feature-box fbox-border fbox-effect">
-                        <div class="fbox-icon">
-                            <a href="#"><i class="icon-tint i-alt"></i></a>
-                        </div>
-                        <h3>Unlimited Color Options</h3>
-                        <p>Change the color scheme of the Theme in a flash just by changing the 6-digit HEX code in the colors.php file.</p>
-                    </div>
-                </div>
-                <div class="col_one_third nobottommargin col_last">
-                    <div class="feature-box fbox-border fbox-effect">
-                        <div class="fbox-icon">
-                            <a href="#"><i class="icon-text-width i-alt"></i></a>
-                        </div>
-                        <h3>CUSTOMIZABLE FONTS</h3>
-                        <p>Use any Font you like from Google Web Fonts, Typekit or other Web Fonts. They will blend in perfectly.</p>
-                    </div>
-                </div>
-            </div>
-    </section>
-
+    <div class="divider"></div>
     <div id="gotoTop" class="icon-angle-up"></div>
 
 <!--============================= footer =============================-->
